9970fea53b131c972b0c06cdaf1c73eb071c6501
unknown <Jim@.(none)>
2009-07-17 00:44:08 -0400

    Manually escaping special characters in emotes

--- emoticons.js
+++ emoticons.js
@@ -1,85 +1,88 @@
-var emoticons = {
-		"emoticon": {
-			"::smile": {
-				"image": "smile.png",
-				"emotes": {
-					":-\)": "",
-					":)": "",
-					":]": "",
-					"=]": "",
-					"=\)": ""
-				}
-			},
-			"::bigSmile": {
-				"image": "bigsmile.png",
-				"emotes": {
-					":D": "",
-					"=D": "",
-					"XD": "",
-					"BD": ""
-				}
-			},
-			"::shock": {
-				"image": "shock.png",
-				"emotes": {
-					":O": "",
-					"=O": "",
-					":(]": ""
-				}
-			},
-			"::frown": {
-				"image": "frown.png",
-				"emotes": {
-					":-(": "",
-					"=(": "",
-					":[": "",
-					":<": "",
-					"=[": "",
-					":(": ""
-				}
-			},
-			"::tongue": {
-				"image": "tongue.png",
-				"emotes": {
-					":P": "",
-					"=P": "",
-					"XP": "",
-				}
-			},
-			"::bored": {
-				"image": "bored.png",
-				"emotes": {
-					"=I": "",
-					":/": "",
-					":-\\": "",
-					":|": ""
-				}
-			},
-			"::wink": {
-				"image": "wink.png",
-				"emotes": {
-					";-)": "",
-					";)": "",
-					";]": ""
-				}
-			},
-			"::love": {
-				"image": "love.png",
-				"emotes": {
-					"<3	": "",
-					"S2": "",
-					":3": ""
-				}
-			}												
-			,
-			"::confused": {
-				"image": "confused.png",
-				"emotes": {
-					":S	": "",
-					"=S": "",
-					":?": ""
-				}
-			}												
-			
-		}
-	};
+var emoticons = {
+	"emoticon": {
+		"::smile": {
+			"image": "smile.png",
+			"emotes": {
+				":-\\)": "",
+				":-\/": "",
+				":\\)": "",
+				":]": "",
+				"=]": "",
+				"=\\)": "",
+				"=\/": ""
+			}
+		},
+		"::bigSmile": {
+			"image": "bigsmile.png",
+			"emotes": {
+				":D": "",
+				"=D": "",
+				"XD": "",
+				"BD": ""
+			}
+		},
+		"::shock": {
+			"image": "shock.png",
+			"emotes": {
+					":O": "",
+					"=O": "",
+					":\\(]": ""
+			}
+		},
+		"::frown": {
+			"image": "frown.png",
+			"emotes": {
+				":-\\(": "",
+				"=\\(": "",
+				":\\[": "",
+				":<": "",
+				":&lt;": "",
+				"=\\[": "",
+				":\\(": ""
+			}
+		},
+		"::tongue": {
+			"image": "tongue.png",
+			"emotes": {
+				":P": "",
+				"=P": "",
+				"XP": "",
+			}
+		},
+		"::bored": {
+			"image": "bored.png",
+			"emotes": {
+				"=I": "",
+//				"://": "",
+//				":-\/": "",
+//  		  ":|": ""
+			}
+		},
+		"::wink": {
+			"image": "wink.png",
+			"emotes": {
+				";-\\)": "",
+				";\\)": "",
+				";\\]": ""
+			}
+		},
+		"::love": {
+			"image": "love.png",
+				"emotes": {
+					"&lt;3": "",
+					"&lt;3": "",
+					"S2": "",
+					":3": ""
+				}
+			}												
+		,
+		"::confused": {
+			"image": "confused.png",
+			"emotes": {
+				":S": "",
+				"=S": "",
+				":\\?": ""
+			}
+		}
+	}
+};
