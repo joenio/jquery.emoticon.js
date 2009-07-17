f316b9e43815b2d07e09945a34e5ad07792ec616
unknown <Jim@.(none)>
2009-07-16 23:57:47 -0400

    first commit

---
+++ emoticons.js
@@ -0,0 +1,85 @@
+var emoticons = {
+		"emoticon": {
+			"::smile": {
+				"image": "smile.png",
+				"emotes": {
+					":-\)": "",
+					":)": "",
+					":]": "",
+					"=]": "",
+					"=\)": ""
+				}
+			},
+			"::bigSmile": {
+				"image": "bigsmile.png",
+				"emotes": {
+					":D": "",
+					"=D": "",
+					"XD": "",
+					"BD": ""
+				}
+			},
+			"::shock": {
+				"image": "shock.png",
+				"emotes": {
+					":O": "",
+					"=O": "",
+					":(]": ""
+				}
+			},
+			"::frown": {
+				"image": "frown.png",
+				"emotes": {
+					":-(": "",
+					"=(": "",
+					":[": "",
+					":<": "",
+					"=[": "",
+					":(": ""
+				}
+			},
+			"::tongue": {
+				"image": "tongue.png",
+				"emotes": {
+					":P": "",
+					"=P": "",
+					"XP": "",
+				}
+			},
+			"::bored": {
+				"image": "bored.png",
+				"emotes": {
+					"=I": "",
+					":/": "",
+					":-\\": "",
+					":|": ""
+				}
+			},
+			"::wink": {
+				"image": "wink.png",
+				"emotes": {
+					";-)": "",
+					";)": "",
+					";]": ""
+				}
+			},
+			"::love": {
+				"image": "love.png",
+				"emotes": {
+					"<3	": "",
+					"S2": "",
+					":3": ""
+				}
+			}												
+			,
+			"::confused": {
+				"image": "confused.png",
+				"emotes": {
+					":S	": "",
+					"=S": "",
+					":?": ""
+				}
+			}												
+			
+		}
+	};
---
+++ jquery.emoticon.js
@@ -0,0 +1,36 @@
+$.fn.emoticon = function(element){
+	
+    	var imagePath = "emotes/";   
+        var ee;
+		
+		for (emote in emoticons.emoticon) {
+			console.log(emoticons.emoticon[emote]);
+			//$(element).append('<img src = "'+imagePath+emoticons.emoticon[emote].image+'" />');
+			for(face in emoticons.emoticon[emote].emotes)
+			{
+				console.log(face);
+				//$(element).append(face);
+			}
+			
+		}
+		
+		
+		
+		/*
+        for(emote in emoticons.emotes)
+        {
+            ee=emoticons.emotes[emote].emotes;
+            for(e in ee)
+                {
+                
+                var k = '$(element).html().replace(/'+e+'/g,"::smile")';
+                eval(k);
+	        console.log(k);
+                $(element).html(k);
+                }
+        }
+*/
+	
+	
+	
+}; 
